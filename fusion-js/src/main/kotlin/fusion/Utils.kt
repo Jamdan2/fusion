eb9b9ad80a37bbf0c2b9e4c27e71b1507d3ae55a
Aidan Heller <asfheller@gmail.com>
2018-08-30 22:10:40 -0700

    Add main render function

--- fusion-js/src/main/kotlin/fusion/Utils.kt
+++ fusion-js/src/main/kotlin/fusion/Utils.kt
@@ -4,7 +4,11 @@ import fusion.dom.Text
 import fusion.dom.Unknown
 import fusion.vdom.VNode
 import fusion.vdom.f
-import fusion.vdom.t
+import fusion.vdom.processVNodeExpandFragments
+import org.w3c.dom.Node
+import kotlin.properties.Delegates
+
+private var rootNode by Delegates.notNull<Node>()
 
 fun ktx(handler: Ktx.() -> Unit): VNode = object : Ktx {
     val children: MutableList<ComponentBase> = arrayListOf()
@@ -27,3 +31,8 @@ fun ktx(handler: Ktx.() -> Unit): VNode = object : Ktx {
         f(*children.map { it.render() }.toTypedArray())
     }
 }.apply(handler).render()
+
+fun render(rootNode: Node, handler: Ktx.() -> Unit) {
+    fusion.rootNode = rootNode
+    rootNode.appendChild(processVNodeExpandFragments(ktx(handler)))
+}
